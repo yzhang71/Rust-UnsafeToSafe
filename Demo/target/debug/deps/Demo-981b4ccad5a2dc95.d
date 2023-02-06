61d5e9e439bb59cd7df257a8886fc3a19bf23e98
yzhang71 <yzhang0701@gmail.com>
2023-02-05 22:13:40 -0500

    Add: demo

---
+++ Demo/target/debug/deps/Demo-981b4ccad5a2dc95.d
@@ -0,0 +1,5 @@
+/home/dennis/Documents/rust-analyzer-UnsafeToSafe/Demo/target/debug/deps/Demo-981b4ccad5a2dc95.rmeta: src/main.rs
+
+/home/dennis/Documents/rust-analyzer-UnsafeToSafe/Demo/target/debug/deps/Demo-981b4ccad5a2dc95.d: src/main.rs
+
+src/main.rs:
---
+++ Demo/target/debug/deps/Demo-b10bb87842d1cc33.d
@@ -0,0 +1,5 @@
+/home/dennis/Documents/rust-analyzer-UnsafeToSafe/Demo/target/debug/deps/Demo-b10bb87842d1cc33.rmeta: src/main.rs
+
+/home/dennis/Documents/rust-analyzer-UnsafeToSafe/Demo/target/debug/deps/Demo-b10bb87842d1cc33.d: src/main.rs
+
+src/main.rs:
